7de24aa021d95b2f74080c016080693aeb025d95
xsaiter <xsaiter@mail.ru>
2017-07-23 10:54:20 +0300

    init

---
+++ main.cc
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+#include <string>
+
+int main(int argc, char *argv[]) {
+  std::ifstream file(argv[1]);
+  std::vector<char> txt((std::istreambuf_iterator<char>(file)),
+                        (std::istreambuf_iterator<char>()));
+  file.close();
+
+  const std::string allowed = "><+-.,[]";
+
+  txt.erase(std::remove_if(txt.begin(), txt.end(), [&](char c) {
+    return allowed.find(c) == std::string::npos;
+  }), txt.end());
+
+  char m[30000];
+  int i = 0;
+
+  int j = 0;
+  const int n = txt.size();
+
+  while (j < n) {
+    char c = txt[j];
+    switch (c) {
+    case '>':
+      ++i;
+      break;
+    case '<':
+      --i;
+      break;
+    case '+':
+      ++m[i];
+      break;
+    case '-':
+      --m[i];
+      break;
+    case '.':
+      std::cout << m[i];
+      break;
+    case ',':
+      std::cin >> m[i];
+      break;
+    case '[':
+      if (m[i] == 0) {
+        int b = 1;
+        while (b != 0) {
+          ++j;
+          if (txt[j] == '[') {
+            ++b;
+          } else if (txt[j] == ']') {
+            --b;
+          }
+        }
+      }
+      break;
+    case ']':
+      if (m[i] != 0) {
+        int b = 1;
+        while (b != 0) {
+          --j;
+          if (txt[j] == ']') {
+            ++b;
+          } else if (txt[j] == '[') {
+            --b;
+          }
+        }
+        --j;
+      }
+      break;
+    }
+    ++j;
+  }
+
+  return 0;
+}
